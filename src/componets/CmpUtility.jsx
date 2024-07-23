cdef1d64afd40d83da553577afa1ac3cd10567ab
AntoCic <anto.cic.127@gmail.com>
2024-07-23 16:42:45 +0200

    First commit

---
+++ src/componets/CmpUtility.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { increment, decrement } from '../redux/counterSlice';
+import { useSelector, useDispatch } from 'react-redux';
+export default function CmpUtility({ children, addTask }) {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch();
+  // const [count, setCount] = useState(0);
+
+  const [inputText, setinputText] = useState('')
+  const handleAddTask = () => {
+    const textToAdd = inputText
+    setinputText('');
+    return addTask(textToAdd)
+  }
+
+  return (
+    <>
+      <button className='mb-8' onClick={() => dispatch(increment())}>
+        {children} {count}
+      </button>
+      <button className='ms-2 px-2' onClick={() => dispatch(decrement())}>Post Precedente
+      </button>
+
+      <hr className='my-3' />
+      <p>ADD TASK: <input value={inputText} className='px-2' onChange={(e) => setinputText(e.target.value)} type="text" placeholder='...' /> <button className='p-0 px-1' onClick={handleAddTask} > + </button></p>
+    </>
+
+  )
+}
